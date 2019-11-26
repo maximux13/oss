3bb52a454a3a51d8c14073621ebb93318352f471
Andres Alarcon <aalarcon@monoku.com>
2019-11-26 17:10:53 -0500

    feat: Export each method individually
    BREAKING CHANGE: Now each method should be call individually

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,13 +1,13 @@
-import math from '../';
+import { sum, substract, times } from '../';
 
 test('sum', () => {
-  expect(math.sum(1, 2, 3)).toBe(6);
+  expect(sum(1, 2, 3)).toBe(6);
 });
 
 test('substract', () => {
-  expect(math.substract(3, 2)).toBe(1);
+  expect(substract(3, 2)).toBe(1);
 });
 
 test('times', () => {
-  expect(math.times(1, 2, 3)).toBe(6);
+  expect(times(1, 2, 3)).toBe(6);
 });
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-const sum = (...args) => {
+export const sum = (...args) => {
   return args.reduce((total, arg) => total + arg, 0);
 };
 
-const substract = (...args) => {
+export const substract = (...args) => {
   return args.slice(1).reduce((total, arg) => total - arg, args[0]);
 };
 
-const times = (...args) => {
+export const times = (...args) => {
   return args.slice(1).reduce((total, arg) => total * arg, args[0]);
-};
-
-export default {
-  sum,
-  substract,
-  times,
 };
